3b428ad964623ba7dcbeed0876fd9e01bc989b88
Marco Cotrufo <marco.cotrufo@devncode.it>
2024-03-29 10:45:35 +0100

    chore: enable thunderbird only on linux

--- home/mail.nix
+++ home/mail.nix
@@ -1,24 +1,34 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    qcal
-  ];
-
-  programs = {
-    mbsync.enable = true;
-    msmtp.enable = true;
-    qcal.enable = true;
+  programs.thunderbird = with pkgs; {
+    enable = stdenv.isLinux;
+    profiles = {
+      marco = {
+        isDefault = true;
+        settings = {
+          "calendar.registry.8cb74868-e955-49ba-a864-0bc5063821ef.type" = "caldav";
+          "calendar.registry.8cb74868-e955-49ba-a864-0bc5063821ef.uri" = "https://calendar.zoho.eu/caldav/zz080112302e8a4359176f396d2394d1bb1c4e5e9d209e19c2fe4c518b96e747ff131be5dac65d21e6621c56071e84d709277e892d/events/";
+          "calendar.registry.8cb74868-e955-49ba-a864-0bc5063821ef.name" = "My Calendar";
+        };
+      };
+    };
   };
 
-  accounts.calendar.basePath = ".calendar";
-  accounts.calendar.accounts = {
-    devncode = {
-      qcal.enable = true;
-      remote = {
-        url = "https://calendar.zoho.eu/caldav/zz080112302e8a4359176f396d2394d1bb1c4e5e9d209e19c2fe4c518b96e747ff131be5dac65d21e6621c56071e84d709277e892d/events/";
-        type = "caldav";
-        passwordCommand = [ "pass" "Email/zoho" ];
-        userName = "marco.cotrufo@runbox.it";
+  accounts.email = with pkgs; {
+    accounts.runbox = {
+      address = "marco.cotrufo@runbox.it";
+      imap.host = "mail.runbox.com";
+      imap.port = 993;
+      userName = "marco.cotrufo@runbox.it";
+      primary = true;
+      realName = "Marco Cotrufo";
+      smtp = {
+        host = "mail.runbox.com";
+        port = 465;
+      };
+
+      thunderbird = {
+        enable = stdenv.isLinux;
       };
     };
   };
