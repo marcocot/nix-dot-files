f8998f7e9a73426e55ee094a1d349aec136bfb7b
Marco Cotrufo <marco.cotrufo@devncode.it>
2024-03-29 11:00:28 +0100

    feat: track p10k configuration

--- home/shell.nix
+++ home/shell.nix
@@ -4,6 +4,13 @@
     zsh
   ];
 
+  home.file = {
+    ".p10k.zsh" = {
+      source = ./p10k.zsh;
+      recursive = true;
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
