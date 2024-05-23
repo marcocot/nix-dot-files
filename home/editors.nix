94b3e433e257410dc8d614a88318d7eb2e37a3ea
Marco Cotrufo <marco.cotrufo@devncode.it>
2024-05-23 08:50:58 +0200

    feat: nvim, add hardtime plugin

--- home/editors.nix
+++ home/editors.nix
@@ -82,6 +82,16 @@
           }
         '';
       };
+      ".config/nvim/lua/plugins/hardtime.lua" = {
+        text = ''
+          return {
+            "m4xshen/hardtime.nvim",
+             dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+             opts = {}
+          }
+        '';
+      };
+
     };
   };
 
